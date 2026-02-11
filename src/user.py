4a27f2e8040b51c313aa7d961f2087c75980b762
Simon Petrequin <simon.petrequin@free.fr>
2026-02-11 13:15:01 +0100

    fix: Add avgSNR to user attributes

--- src/user.py
+++ src/user.py
@@ -2,9 +2,10 @@ from buffer import Buffer
 from packet import Packet, PacketGenerator
 
 class User:
-    def __init__(self, id):
+    def __init__(self, id, avgSNR):
         self.id = id
-        self.buffer:Buffer = Buffer()  # Initialize with a buffer of 1000 bits
+        self.buffer:Buffer = Buffer(1000)  # Initialize with a buffer of 1000 bits
+        self.avgSNR = avgSNR
 
     def consume_packet(self, packet):
         self.buffer.pop(packet)  # Consume the packet from the buffer
