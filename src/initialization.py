6b434ff8c8f6dba0f6f2c207a038aa0c2c3fa55f
Simon Petrequin <simon.petrequin@free.fr>
2026-02-11 18:07:53 +0100

    feat: Integrate current work into the main pipeline

--- src/initialization.py
+++ src/initialization.py
@@ -1,8 +1,10 @@
 import math
+from user import User
 
 def init(n: int):
     users = []
     for i in range(math.floor(n/2)):
         # Generate the same amount of close and far users
-        users.append(User())
+        users.append(User(2*i, 7)) # far users
+        users.append(User(2*i+1, 11)) # close users
     return users
--- src/main.py
+++ src/main.py
@@ -3,13 +3,15 @@ from scheduler import Scheduler
 from src.initialization import init
 import sys
 
+from user import User
+
 def main(max_ticks: int, nb_users: int, algorithm: str):
     print("Initializing simulation")
     print(f"Simulation duration: {max_ticks} ticks")
     print(f"User number: {nb_users}")
     print(f"Algorithm used: {algorithm}")
 
-    users = init(nb_users)
+    users: list[User] = init(nb_users)
 
     packet_gen = PacketGenerator(1000, 100) # 1000 bits/tick max, 100 paquet size # Bits/ticks calculé selon notre objectif de crash
     scheduler = Scheduler(algorithm)
@@ -18,11 +20,11 @@ def main(max_ticks: int, nb_users: int, algorithm: str):
     print("Starting simulation...")
     while (tick < max_ticks) :
         packet_gen.generateUsersPackets(users, tick) # Generate packets in each user's buffer
-        scheduler.apply() # Donner les UR aux users
-        apply_scheduling() # Vider les paquets utilisés 
+        updates: list[tuple[User, int]] = scheduler.select_repartition(users) # Donner les UR aux users
+        scheduler.apply_repartition(updates) # Vider les paquets utilisés 
         sample() # Prendre les mesures
         tick += 1
-
+    print("Simulation successfully ended !")
     display_stats()
 
 
