9f8e9976da57feaaf73ccbf7866482a323b41e30
Simon Petrequin <simon.petrequin@free.fr>
2026-03-23 10:24:15 +0100

    feat: Add mesurements to simulation

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
+from functools import reduce
 import sys
 
 from initialization import init
+from mesures import record_ur_usage, generate_plots
 from packet import PacketGenerator
 from scheduler import Scheduler
-from user import User
-
+from user import DUMMY_USER, User
 
 def main(max_ticks: int, nb_users: int, algorithm: str):
+
     print("Initializing simulation")
     print(f"Simulation duration: {max_ticks} ticks")
     print(f"User number: {nb_users}")
@@ -15,12 +17,12 @@ def main(max_ticks: int, nb_users: int, algorithm: str):
     users: list[User] = init(nb_users)
 
     packet_gen = PacketGenerator(
-        1000, 100
+        1000, 50
     )  # 1000 bits/tick max, 100 paquet size # Bits/ticks calculé selon notre objectif de crash
     scheduler = Scheduler(algorithm)
 
-    tick = 0
     print("Starting simulation...")
+    tick = 0
     while tick < max_ticks:
         packet_gen.generateUsersPackets(
             users, tick
@@ -28,11 +30,15 @@ def main(max_ticks: int, nb_users: int, algorithm: str):
         updates: list[tuple[User, int]] = scheduler.select_repartition(
             users
         )  # Donner les UR aux users
-        scheduler.apply_repartition(updates)  # Vider les paquets utilisés
-        # sample() # Prendre les mesures
+
+        scheduler.apply_repartition(updates, tick) # Vider les paquets utilisés
+        
+        # record mesures
+        nb_used_ur = reduce(lambda acc, e: (acc+1) if e[0] != DUMMY_USER else acc, updates, 0)
+        record_ur_usage(nb_used_ur, len(updates))
         tick += 1
     print("Simulation successfully ended !")
-    # display_stats()
+    generate_plots(0)
 
 
 if __name__ == "__main__":
