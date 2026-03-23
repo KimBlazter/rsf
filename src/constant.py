8b4717ccbe36ff7aed4be9fd1af05ba6707bbfec
Simon Petrequin <simon.petrequin@free.fr>
2026-03-24 00:19:11 +0100

    fix: Tweak simulator configuration

--- src/constant.py
+++ src/constant.py
@@ -1,5 +1,5 @@
-PROCHE_AVG_SNR = 11
-LOIN_AVG_SNR = 7
-BUFFER_SIZE = 1000000
-BITS_PER_SNR_POINT = 10
-MAX_UR = 640 # 128 * 5
+PROCHE_AVG_SNR = 12 # SNR for close users
+LOIN_AVG_SNR = 4 # SNR for far users
+BUFFER_SIZE = 50000 # max bits each we can store to send to each users (if more bits comes in they will be discarded)
+BITS_PER_SNR_POINT = 80 # when allocatin a UR, how much bits do we remove from buffer per SNR point
+MAX_UR = 640 # 128 * 5 # how much UR are available each tick
