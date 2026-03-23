e54d9f4f0aa9e23f99988d2dde28fab5cfdf8308
Simon Petrequin <simon.petrequin@free.fr>
2026-03-23 18:48:59 +0100

    feat: Improve project configuration

--- src/user.py
+++ src/user.py
@@ -1,4 +1,5 @@
 from buffer import Buffer
+from constant import BUFFER_SIZE
 from packet import Packet
 import sys
 from mesures import record_bits, record_delay
@@ -6,11 +7,12 @@ from mesures import record_bits, record_delay
 class User:
     def __init__(self, id, avgSNR):
         self.id = id
-        self.buffer:Buffer = Buffer(1000)  # Initialize with a buffer of 1000 bits
+        self.buffer:Buffer = Buffer(BUFFER_SIZE)  # Initialize with a buffer of 1000 bits
         self.avgSNR = avgSNR
 
     def _add_packet(self, packet: Packet) -> None:
         self.buffer.push(packet)
+            
     
     def add_packets(self, packets: list[Packet]) -> None:
         for p in packets:
@@ -19,8 +21,7 @@ class User:
     def allocate_bits(self, bits: int, curr_tick: int) -> None:
         _ , delay, nb_packets = self.buffer.pop(bits, curr_tick)
         record_bits(bits, self.avgSNR)
-        if nb_packets  == 0:
-            return
-        record_delay(delay/nb_packets, self.avgSNR)
+        for _ in range(nb_packets):
+            record_delay(delay/nb_packets, self.avgSNR, curr_tick)
 
 DUMMY_USER = User(-1, sys.maxsize)
