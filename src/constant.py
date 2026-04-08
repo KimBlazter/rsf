07f2991889e00912bb1abca3f608aefd815d9505
Saunier Martin <martin.saunier@etudiant.univ-rennes.fr>
2026-04-08 11:22:17 +0200

    feat: Add PDOR_THRESHOLD constant

--- src/constant.py
+++ src/constant.py
@@ -4,6 +4,8 @@ LOIN_AVG_SNR = 4 # SNR for far users
 LOW_RELAY_RATIO = 0.1
 HIGH_RELAY_RATIO = 0.8
 
+PDOR_THRESHOLD = 50
+
 BITS_PER_SNR_POINT = 80 # when allocatin a UR, how much bits do we remove from buffer per SNR point
 MAX_UR = 640 # 128 * 5 # how much UR are available each tick
 PACKET_SIZE = 50
